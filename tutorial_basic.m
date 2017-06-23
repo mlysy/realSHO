a859b1bd9bbe64b02d8e61bedb488224f7b83170
byates6 <bryanyates@outlook.com>
2017-06-23 09:56:50 -0400

    updating for use with get_periodogram.m

--- tutorial_basic.m
+++ tutorial_basic.m
@@ -36,24 +36,7 @@ yAPSD = yAPSD + Aw_s;
 
 % TIME DOMAIN
 [xTime, yTime] = tsSim(SF_s, SF_s*T_s, xAPSD, yAPSD);
-
-% fft(yTime)
-N = length(yTime)-rem(length(yTime),2); % use even PSD's only
-FR = SF_s/N;
-yTime = yTime(1:N);
-yFreq = fft(yTime)/sqrt(SF_S*N); % CLT for fft
-% Remove DC component, select first frequency up to the Nyquist frequency.
-% Drop negative frequencies. For a real signal, negative frequencies are redundant.
-yFreq = yFreq(2:N/2+1);
-xFreq = linspace(1/T_s, SF_s/2, length(yFreq));
-
-yFreq(end) = real(yFreq(end)); % Nyquist freq should be real for real signal
-yFreq = abs(yFreq).^2; % Take the squared magnitude at each frequency
-% Make the PSD single-sided by doubling everything except the Nyquist frequency.
-% This corrects for the loss in amplitude caused by dropping negative frequencies
-% the Nyquist frequency at N/2 doesn't undergo folding, and therefore is NOT doubled.
-yFreq(1:N/2-1) = 2.*yFreq(1:N/2-1);
-
+[xFreq,yFreq] = get_periodogram(yTime, SF_s, T_s);
 
 %% Visual in both time and frequency domain
 % Perform binning top help visuals
